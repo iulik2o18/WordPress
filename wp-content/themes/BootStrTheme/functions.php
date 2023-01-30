215bcde7378061eb1677c855f505783fb77a358c
I.Spataru <iulion08@gmail.com>
2023-01-30 09:23:33 +0000

    WordPress

---
+++ wp-content/themes/BootStrTheme/functions.php
@@ -0,0 +1,43 @@
+<?php
+
+function bootstr_theme_support()
+{
+    // Dynamic Title tag
+    add_theme_support('title-tag');
+    add_theme_support('custom-logo');
+    add_theme_support('post-thumbnails');
+}
+
+add_action('after_setup_theme','bootstr_theme_support');
+
+function boostr_menus(){
+    $location = array(
+        'primary' => 'Desktop Primary Left Side Bar',
+        'footer' => 'Footer menu items'
+    );
+
+    register_nav_menus($location);
+}
+
+add_action('init','boostr_menus');
+
+function bootstr_register_styles()
+{
+    $version = wp_get_theme()->get('Version');
+    wp_enqueue_style('bootstr_style', get_template_directory_uri() . "/style.css", array('bootstr_bootstrap'), $version, 'all');
+    wp_enqueue_style('bootstr_bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css', array(), "5.3.0", "all");
+    wp_enqueue_style('bootstr_fontawesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css', array(), "6.2.1", "all");
+}
+
+add_action('wp_enqueue_scripts', 'bootstr_register_styles');
+
+function bootstr_register_scripts()
+{
+
+    wp_enqueue_script('bootstr_jquery', "https://code.jquery.com/jquery-3.6.3.min.js", array(), '3.6.3', true);
+    wp_enqueue_script('bootstr_popper', "https://unpkg.com/@popperjs/core@2", array(), '2.0', true);
+    wp_enqueue_script('bootstr_bootstrapjs', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js', array(), "5.3.0", true);
+    wp_enqueue_script('bootstr_script', get_template_directory_uri() . "/main.js", array(), '1.0', true);
+}
+
+add_action('wp_enqueue_scripts', 'bootstr_register_scripts');
---
+++ wp-content/themes/BootStrTheme/header.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Blog Site Template">
+    <meta name="author" content="https://youtube.com/FollowAndrew">
+    <link rel="shortcut icon" href="/wp-content/themes/BootStrTheme/assets/images/logo.png">
+
+    <?php wp_head(); ?>
+
+</head>
+
+<body>
+
+    <header class="header text-center">
+        <a class="site-title pt-lg-4 mb-0" href="index.html"><?= get_bloginfo('name'); ?></a>
+
+        <nav class="navbar navbar-expand-lg navbar-dark">
+            
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div id="navigation" class="collapse navbar-collapse flex-column">
+                <?php
+                
+                if(function_exists('the_custom_logo')){
+                    $custom_logo_id = get_theme_mod('custom_logo');
+                    $logo = wp_get_attachment_image_src($custom_logo_id);
+                }
+                ?>
+
+                <img class="mb-3 max-auto logo" src="<?= $logo[0] ?>" alt="">
+
+                <?php 
+                
+                wp_nav_menu(
+                    [
+                        'menu' => 'primary',
+                        'container' => '',
+                        'theme_location' => 'primary',
+                        'items_wrap' => '<ul class = "navbar-nav flex-column text-sm-center text-md-left">%3$s</ul>'
+                    ]
+                );
+                
+                ?>
+
+            </div>
+        </nav>
+    </header>
+    <div class="main-wrapper">
+        <header class="page-title theme-bg-light text-center gradient py-5">
+            <h1 class="heading"><?php the_title(); ?></h1>
+        </header>
